0c17078f81ac935944fd2fcf1044d80b27df9eea
Sarvesh Sambhaji Gawade <sarveshgawadesg@gmail.com>
2025-06-04 16:35:51 +0530

    initialized frontend in react using vite

---
+++ Backend/config/dbConnection.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { config } from 'dotenv';
+config()
+
+
+// kepping less strictness if wrong query occur  
+mongoose.set('strictQuery',false)
+
+const connectToDB = async () =>{
+    try {
+        const {connection} = await mongoose.connect(process.env.MONGODB_ATLAS_URL)
+        console.log(`connected with DB: ${connection.host}`);
+    } catch (error) {
+        console.log(`Error  in connecting with DB: ${error}`);
+        process.exit(1)
+    }
+}
+
+export default connectToDB
