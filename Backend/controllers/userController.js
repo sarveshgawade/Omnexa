bf51dc6c4b2849e4794407d11ba5668c935f103f
Sarvesh Sambhaji Gawade <sarveshgawadesg@gmail.com>
2025-06-07 22:16:01 +0530

    added login controller and login route

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -36,60 +36,8 @@ const register = async(req,res,) =>{
         return res.status(500).json('User registration failed !')
     }
 
-    // IMAGE  FILE UPLOAD
-    // if(req.file){
-    //     // console.log(req.file);
-    //     try {
-    //         const result = await cloudinary.v2.uploader.upload(req.file.path,{
-    //             folder: 'lms',
-    //             width: 250,
-    //             height: 250 ,
-    //             gravity: 'faces',
-    //             crop: 'fill'
-    //         })
-
-    //         if(result){
-    //             newUser.avatar.public_id = result.public_id 
-    //             newUser.avatar.secure_url = result.secure_url 
-
-    //             // remove file from server
-    //             fs.rm(`uploads/${req.file.filename}`)
-    //         }
-    //     } catch (error) {
-    //         return res.status(500).json('Error in uploading image !')
-    //     }
-    // }
-
-
     // save user in DB
     await newUser.save()
-
-    //Register SuccessFully Mail
-//     try {
-//         const subject = `Welcome to Alumni-Next - Your Ultimate Hub for Alumni & Students at Thakur College of Science & Commerce`;
-// const message = `
-//     <h3> Dear ${fullName}, </h3><br>
-//     <br>We are thrilled to welcome you to Alumni-Next, the official platform connecting alumni and students of Thakur College of Science & Commerce, Thakur Village, Kandivali! 🎉🎊
-//     <br><br>
-//     <p>Your registration has been successfully completed, and you are now part of a vibrant community where alumni and students can connect, collaborate, and grow together. Whether you are here to seek career guidance, post job or internship opportunities, or participate in various events and reunions, we have something for everyone!</p>
-//     <br>
-//     <p>Students can benefit from the wealth of experience shared by alumni, while alumni can offer their expertise, share insights, and post opportunities to help shape the future generation of Thakur College graduates.</p>
-//     <br>
-//     <p>If you have any questions or need assistance, feel free to reach out to us at: </p>
-//     <ul>
-//         <li>Tel: 2887 0627, 2846 2565</li>
-//         <li>Fax: 2886 8822</li>
-//     </ul>
-//     <br>
-//     <p>We look forward to fostering meaningful connections and supporting both alumni and students on this platform. Welcome aboard!</p>
-// `;
-
-
-//        sendMail(email,subject,message)
-//     } catch (error) {
-//         console.error('Error :',error)
-//     }
-
 
     // token generation
     const token = await newUser.generateJWTtoken()
@@ -108,5 +56,44 @@ const register = async(req,res,) =>{
     })
 }
 
+const login = async (req,res) =>{
+    // console.log("eeeeeeeeeeeeeeeeee");
+    
+    try {
+        const {email,password} = req.body
 
-export {register}
+        if(!email || !password){
+            return res.status(500).json('All fields are required !') 
+        }
+
+        // getting password explicitly because it was selected as false in schema
+        const existingUser = await User.findOne({
+            email
+        }).select('+password')  
+
+        if(!existingUser || !(await existingUser.comparePassword(password))){
+            return res.status(500).json('Email & password wont match !') 
+        }
+
+        const token = await existingUser.generateJWTtoken()
+        res.cookie('token',token,cookieOptions)
+
+        existingUser.password = undefined
+        
+        res.status(200).json({
+            success: true,
+            message: `User logged in successfully`,
+            existingUser
+        })
+    } catch (e) {
+        return res.status(500).json({
+            success: false ,
+            message: e.message
+        })
+    }
+    
+
+}
+
+
+export {register,login}
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,11 +1,11 @@
 import {Router} from 'express'
-import {register} from '../controllers/userController.js'
+import {register,login} from '../controllers/userController.js'
 import {  authorizedRoles, isLoggedIn } from '../middlewares/authMiddleware.js'
 
 const router = Router()
 
 router.post('/register',register)
-// router.post('/login',login)
+router.post('/login',login)
 // router.get('/my-profile',isLoggedIn,getProfile)
 // router.get('/logout',isLoggedIn,logout)
 // router.post('/change-password',isLoggedIn,changePassword)
