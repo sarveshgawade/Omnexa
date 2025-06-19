58a93508e786fc427deb1bf8638dd778c97f025f
Sarvesh Sambhaji Gawade <sarveshgawadesg@gmail.com>
2025-06-19 11:35:50 +0530

    added dev.js vercel.json

--- Backend/app.js
+++ Backend/app.js
@@ -1,3 +1,5 @@
+import { configDotenv } from 'dotenv'
+configDotenv()
 import express from 'express'
 import connectToDB from './config/dbConnection.js'
 import morgan from 'morgan'
@@ -5,6 +7,7 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import {userRoutes,productRoutes,quoteRoutes} from './routes/routes.index.js'
 import errorMiddlware from './middlewares/errorMiddleware.js'
+import cloudinary from './config/cloudinary.js'
 
 
 // taking app instance
@@ -15,11 +18,9 @@ const app = express()
 // database connection
 connectToDB()
 
-
-
 // middlewares
 const corsOptions = {
-    origin : "http://localhost:5173",
+    origin : process.env.FRONTEND_URL,
     credentials : true
 }
 
@@ -29,9 +30,15 @@ app.use(cors(corsOptions))
 app.use(cookieParser())
 app.use(morgan('dev'))
 
+
+// test api's
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from Vercel API' });
 });
+app.get('/', (req, res) => {
+  res.json({ message: 'Homepage' });
+});
+
 
 
 // api's
