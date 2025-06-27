15c3fbd19083ad3bdbf4cd1af1fdab0d890227fa
Sarvesh Sambhaji Gawade <sarveshgawadesg@gmail.com>
2025-06-27 19:19:07 +0530

    completed with user register module

--- Frontend/src/pages/RegisterPage.tsx
+++ Frontend/src/pages/RegisterPage.tsx
@@ -1,5 +1,5 @@
 import BaseLayout from '@/layouts/BaseLayout'
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Card,
   CardAction,
@@ -12,11 +12,132 @@ import {
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@radix-ui/react-label'
-import { useNavigate } from 'react-router-dom'
+import { data, useNavigate } from 'react-router-dom'
+import { toast } from 'sonner'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { useDispatch } from 'react-redux'
+import { signUp } from '@/redux/slices/authSlice'
+import type { AppDispatch } from '@/redux/store'
+
 
 function RegisterPage() {
 
+  type RegisterFormDataType = {
+      fullName: string,
+      email: string,
+      phoneNumber: string,
+      password: string,
+      countryIso : String
+  };
+
+  const countryCodes = [
+  { name: "India", code: "+91", iso: "IN" },
+  { name: "United States", code: "+1", iso: "US" },
+  { name: "United Kingdom", code: "+44", iso: "GB" },
+  { name: "Canada", code: "+1", iso: "CA" },
+  { name: "Australia", code: "+61", iso: "AU" },
+  { name: "Germany", code: "+49", iso: "DE" },
+  { name: "France", code: "+33", iso: "FR" },
+  { name: "UAE", code: "+971", iso: "AE" },
+  { name: "Singapore", code: "+65", iso: "SG" },
+  { name: "Japan", code: "+81", iso: "JP" },
+];
+
   const navigate = useNavigate()
+  const dispatch = useDispatch<AppDispatch>()
+  const [formData, setFormData] = useState({
+    fullName: '',
+    email: '',
+    phoneNumber: '',
+    password: '',
+    countryIso: '',
+  })
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>){
+    setFormData({
+      ...formData,
+      [e.target.id] : e.target.value
+    })
+  }
+
+  async function handleSubmit(){
+  
+    if(validateForm(formData)){
+
+      const formDataToSend = {
+        fullName: formData.fullName,
+        email: formData.email ,
+        phoneNumber: formData.countryIso.split('-')[0] +  "-" +formData.phoneNumber,
+        password: formData.password
+      }
+
+      const response = await dispatch(signUp(formDataToSend))
+      
+      if(response?.payload?.success){
+
+          toast.success('Please login to proceed !')
+
+          navigate('/login')
+
+           setFormData({
+            fullName: '',
+            email: '',
+            phoneNumber: '',
+            password: '',
+            countryIso: '',
+          })
+      }
+
+    }      
+   
+  }
+
+  function validateForm(formData:RegisterFormDataType) : boolean{
+
+    if(!formData.fullName ){
+        toast.error('Full name is a required field  !')
+        return false
+    }
+
+    if(!formData.email){
+      toast.error('Email is a required field !')
+        return false
+    }
+    if(!formData.phoneNumber ){
+      toast.error('Phone Number is a required field !')
+        return false
+    }
+    if( !formData.password){
+      toast.error('Password is a required field !')
+        return false
+    }
+    if(  !formData.countryIso){
+      toast.error('Country ISO Code is a required field !')
+        return false
+    }
+
+    if(formData.fullName.length < 6){
+      toast.error('Full name should be atleast of 5 characters!')
+      return false
+    }
+
+    if(!formData.password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#^])[A-Za-z\d@$!%*?&#^]{8,}$/)){
+      toast.error('Enter a strong password !')
+      return false
+    }
+
+    if(!formData.email.match(/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/)){
+      toast.error('Enter a valid email !')
+      return false
+    }
+
+    if(formData.phoneNumber.length < 5){
+      toast.error('Enter a valid phone number !')
+      return false
+    }
+
+    return true
+  }
 
   return (
     <BaseLayout>
@@ -42,6 +163,8 @@ function RegisterPage() {
                     type="text"
                     placeholder="John Doe"
                     required
+                    onChange={handleChange}
+                    value={formData.fullName}
                   />
                 </div>
                 <div className="grid gap-2">
@@ -51,24 +174,59 @@ function RegisterPage() {
                     type="email"
                     placeholder="john@example.com"
                     required
+                    onChange={handleChange}
+                    value={formData.email}
                   />
                 </div>
-                <div className="grid gap-2">
+               <div className="grid gap-2">
                   <Label htmlFor="phoneNumber">Phone Number</Label>
-                  <Input
-                    id="phoneNumber"
-                    type="tel"
-                    placeholder="+911234567890"
-                    required
-                  />
+                  <div className="flex gap-2">
+                    {/* Country Code Dropdown */}
+                    <div className="w-[120px]">
+                      <Select onValueChange={(value) => setFormData({ ...formData, countryIso: value })} value={formData.countryIso}>
+                        <SelectTrigger className="w-full">
+                          <SelectValue placeholder="+91" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {countryCodes.map(({ name, code, iso }) => (
+                            <SelectItem key={`${code}-${iso}`} value={`${code}-${iso}`}>
+                              <div className="flex items-center gap-2">
+                                <img
+                                  src={`https://flagcdn.com/w20/${iso.toLowerCase()}.png`}
+                                  alt={name}
+                                  className="w-5 h-4 rounded-sm object-cover"
+                                />
+                                <span className="text-sm">{code}</span>
+                              </div>
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                    </div>
+
+                    {/* Phone Number Input */}
+                    <Input
+                      id="phoneNumber"
+                      type="tel"
+                      placeholder="1234567890"
+                      required
+                      onChange={handleChange}
+                      value={formData.phoneNumber}
+                      className="flex-1"
+                    />
+                  </div>
                 </div>
+
+
+
                 <div className="grid gap-2">
                   <Label htmlFor="password">Password</Label>
                   <Input
                     id="password"
                     type="password"
-                    placeholder="••••••••"
                     required
+                    onChange={handleChange}
+                    value={formData.password}
                   />
                 </div>
               </div>
@@ -76,7 +234,7 @@ function RegisterPage() {
           </CardContent>
 
           <CardFooter className="flex-col gap-2">
-            <Button type="submit" className="w-full cursor-pointer">
+            <Button onClick={handleSubmit} className="w-full cursor-pointer" >
               Register
             </Button>
           </CardFooter>
--- Frontend/src/redux/slices/authSlice.ts
+++ Frontend/src/redux/slices/authSlice.ts
@@ -2,112 +2,124 @@ import { createAsyncThunk, createSlice} from '@reduxjs/toolkit'
 import axiosInstance from '../../helpers/axiosInstance'
 import {toast} from 'sonner'
 
-const initialState = {
-    isLoggedIn : localStorage.getItem('isLoggedIn') || false ,
-    role : localStorage.getItem('role') || '' ,
-    data : localStorage.getItem('data') || {}
+
+interface AuthState{
+    isLoggedIn: boolean,
+    role : 'USER' | 'ADMIN',
+    data : {}
 }
 
-export const signUp = createAsyncThunk('auth/signup', async function (data){
-    try {
-        // console.log(data.get('avatar'));
-        
-        const response = axiosInstance.post('/user/register',data)
+const initialState: AuthState = {
+  isLoggedIn: localStorage.getItem('isLoggedIn') === 'true',
+  role: (localStorage.getItem('role') as 'USER' | 'ADMIN') || 'USER',
+  data: localStorage.getItem('data') ? JSON.parse(localStorage.getItem('data')!) : {},
+};
 
+
+type RegisterFormDataType = {
+  fullName: string;
+  email: string;
+  phoneNumber: string;
+  password: string;
+};
+
+
+export const signUp = createAsyncThunk('auth/signup', async function (data : RegisterFormDataType){
+    try {
+        const response = axiosInstance.post('/api/v1/user/register',data )
+
+        
         toast.promise(response,{
             loading: 'Creating account ...',
-            error: 'Error in creating account !'
-            ,
-            success: (data) => {
-                return data?.data?.message
-            }
-        })
-
-        return await response
-
-    } catch (error) {
-        console.log(error);
-    }
-})
-
-export const signin = createAsyncThunk('/auth/signin',async function (data) {
-    try {
-        const response = axiosInstance.post('/user/login',data)
-
-        // console.log(data);
-        
-
-        toast.promise(response,{
-            loading: 'Authenticating ...',
-            success: (data)=> {
-                return data?.data?.message
-            },
-            error: (data)=> {
-                return data?.response?.data                 
-            }
-        })
-
-        return (await response).data
-    } catch (error) {
-        console.log(error);
-    }
-})
-
-export const signout = createAsyncThunk('/auth/signout', async function () {
-    try {
-        const response = axiosInstance.get('/user/logout')
-
-        // console.log(`res in slide --> ${(await response).data}`);
-
-        toast.promise(response, {
-            loading: 'Logging out ...',
-            success: (data)=> {
-                return data?.data?.message
-            },
-            error: (data) => data?.data?.message
+            error: (d) => d?.response?.data?.message,
+            success: (d) => d?.data
         })
         
         return (await response).data
-        
-    } catch (error) {
-        console.log(error);
-        
-    }
-})
-
-export const getProfile = createAsyncThunk('/auth/getProfile', async function () {
-    try {
-        const response = axiosInstance.get('/user/my-profile')
-        // console.log((await response).data.userProfile)
-
-        return (await response).data.userProfile
 
     } catch (error) {
         console.log(error);
-        
     }
 })
 
-export const updateProfile = createAsyncThunk('/user/update', async function (data) {
-        try {
+// export const signin = createAsyncThunk('/auth/signin',async function (data) {
+//     try {
+//         const response = axiosInstance.post('/user/login',data)
 
-            // console.log(data);
-            
-            const response =  axiosInstance.post('/user/update',data)
+//         // console.log(data);
+        
 
-            toast.promise(response,{
-                loading: 'Updating ...',
-                error: 'Error while updating',
-                success : d => d?.data?.message
-            })
+//         toast.promise(response,{
+//             loading: 'Authenticating ...',
+//             success: (data)=> {
+//                 return data?.data?.message
+//             },
+//             error: (data)=> {
+//                 return data?.response?.data                 
+//             }
+//         })
 
-            return ((await response).data)
+//         return (await response).data
+//     } catch (error) {
+//         console.log(error);
+//     }
+// })
+
+// export const signout = createAsyncThunk('/auth/signout', async function () {
+//     try {
+//         const response = axiosInstance.get('/user/logout')
+
+//         // console.log(`res in slide --> ${(await response).data}`);
+
+//         toast.promise(response, {
+//             loading: 'Logging out ...',
+//             success: (data)=> {
+//                 return data?.data?.message
+//             },
+//             error: (data) => data?.data?.message
+//         })
+        
+//         return (await response).data
+        
+//     } catch (error) {
+//         console.log(error);
+        
+//     }
+// })
+
+// export const getProfile = createAsyncThunk('/auth/getProfile', async function () {
+//     try {
+//         const response = axiosInstance.get('/user/my-profile')
+//         // console.log((await response).data.userProfile)
+
+//         return (await response).data.userProfile
+
+//     } catch (error) {
+//         console.log(error);
+        
+//     }
+// })
+
+// export const updateProfile = createAsyncThunk('/user/update', async function (data) {
+//         try {
+
+//             // console.log(data);
             
-        } catch (error) {
-            console.log(error);
+//             const response =  axiosInstance.post('/user/update',data)
+
+//             toast.promise(response,{
+//                 loading: 'Updating ...',
+//                 error: 'Error while updating',
+//                 success : d => d?.data?.message
+//             })
+
+//             return ((await response).data)
             
-        }
-})
+//         } catch (error) {
+//             console.log(error);
+            
+//         }
+// })
 
 
 const authSlice = createSlice({
@@ -116,31 +128,31 @@ const authSlice = createSlice({
     reducers:{},
     extraReducers: (builder) => {
         builder
-        .addCase(signin.fulfilled, (state,action)=> {
-            localStorage.setItem('data',JSON.stringify(action?.payload))
-            localStorage.setItem('role',action?.payload?.existingUser?.role)
-            localStorage.setItem('isLoggedIn',true)
+        // .addCase(signin.fulfilled, (state,action)=> {
+        //     localStorage.setItem('data',JSON.stringify(action?.payload))
+        //     localStorage.setItem('role',action?.payload?.existingUser?.role)
+        //     localStorage.setItem('isLoggedIn',true)
 
-            state.isLoggedIn = true
-            state.data = action?.payload
-            state.role = action?.payload?.existingUser?.role 
-        })
-        .addCase(signout.fulfilled, (state) => {
-            localStorage.clear()
+        //     state.isLoggedIn = true
+        //     state.data = action?.payload
+        //     state.role = action?.payload?.existingUser?.role 
+        // })
+        // .addCase(signout.fulfilled, (state) => {
+        //     localStorage.clear()
 
-            state.isLoggedIn = false
-            state.role = ''
-            state.data = {}
-        })
-        .addCase(getProfile.fulfilled, (state, action) => {
-            localStorage.setItem('data', action?.payload)
-            localStorage.setItem('isLoggedIn',true)
-            localStorage.setItem('role',action?.payload?.role)
+        //     state.isLoggedIn = false
+        //     state.role = ''
+        //     state.data = {}
+        // })
+        // .addCase(getProfile.fulfilled, (state, action) => {
+        //     localStorage.setItem('data', action?.payload)
+        //     localStorage.setItem('isLoggedIn',true)
+        //     localStorage.setItem('role',action?.payload?.role)
 
-            state.isLoggedIn = true
-            state.data = action?.payload
-            state.role = action?.payload?.role
-        })
+        //     state.isLoggedIn = true
+        //     state.data = action?.payload
+        //     state.role = action?.payload?.role
+        // })
     }
 })
 
