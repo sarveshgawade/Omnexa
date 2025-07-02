5373c54d01be115d1091b71875aac2d6d37e28c9
Sarvesh Sambhaji Gawade <sarveshgawadesg@gmail.com>
2025-07-02 12:03:12 +0530

    updated prod mongo url

--- Backend/config/dbConnection.js
+++ Backend/config/dbConnection.js
@@ -8,7 +8,7 @@ mongoose.set('strictQuery',false)
 
 const connectToDB = async () =>{
     try {
-        const {connection} = await mongoose.connect(process.env.MONGODB_URL_DEV)
+        const {connection} = await mongoose.connect(process.env.MONGODB_URL_PROD)
         console.log(`connected with DB: ${connection.host}`);
     } catch (error) {
         console.log(`Error  in connecting with DB: ${error}`);
