10eec6c82ba951a4f537da0bbce5eb1588493968
Sarvesh Sambhaji Gawade <sarveshgawadesg@gmail.com>
2025-07-21 16:12:24 +0530

    added custom domain in app.js cors

--- Backend/app.js
+++ Backend/app.js
@@ -20,7 +20,7 @@ connectToDB()
 
 // middlewares
 const corsOptions = {
-    origin : [process.env.FRONTEND_URL_PROD,process.env.FRONTEND_URL_DEV],
+    origin : [process.env.FRONTEND_URL_PROD,process.env.FRONTEND_URL_DEV,process.env.FRONTEND_URL_PROD_WITH_CUSTOM_DOMAIN],
     credentials : true
 }
 
